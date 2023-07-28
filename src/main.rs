0d90b2227e0fa91872c2483066bbc4210cb8d729
Exotik850 <kidkool850@gmail.com>
2023-07-28 16:45:08 -0500

    testing customer query

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,12 @@ mod functions;
 mod quickbook;
 use functions::query::QBQuery;
 use quickbook::Quickbooks;
-use quickbooks_types::models::Invoice;
+use quickbooks_types::models::{Invoice, Customer};
 
 #[tokio::main]
 async fn main() {
     let qb =
         Quickbooks::new_from_env("4620816365257778210", intuit_oauth::Environment::SANDBOX).await.unwrap();
-    let invs = Invoice::query(&qb, r#"where DocNumber = '1010'"#).await.unwrap();
+    let invs = Customer::query(&qb, r#"where GivenName = 'John' and FamilyName = 'Melton'"#).await.unwrap();
     println!("{}", invs[0])
 }
