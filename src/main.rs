0ed4cf10932c8f66844abd4319df7d4614ee8ffe
Exotik850 <kidkool850@gmail.com>
2023-07-31 13:53:06 -0500

    Fixed RA settings

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,14 @@ use quickbooks_types::models::Customer;
 
 #[tokio::main]
 async fn main() {
+    let start = std::time::Instant::now();
     let qb = Quickbooks::new_from_env("4620816365257778210", intuit_oxi_auth::Environment::SANDBOX)
         .await
         .unwrap();
     let invs = Customer::query(&qb, r#"where GivenName = 'John' and FamilyName = 'Melton'"#)
         .await
         .unwrap();
-    println!("{}", invs[0])
+    let end = start.elapsed();
+    println!("{}", invs[0]);
+    eprintln!("Done in {end:?}")
 }
