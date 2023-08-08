ac3c6e325cc152952dd288f2102ef40ade425413
Exotik850 <kidkool850@gmail.com>
2023-08-08 15:30:24 -0500

    PDF Function implementation working

--- src/functions/pdfable.rs
+++ src/functions/pdfable.rs
@@ -1,37 +1,58 @@
 use async_trait::async_trait;
 use intuit_oxi_auth::Authorized;
-use quickbooks_types::{QBItem, QBPDFable, QBReadable};
+use quickbooks_types::{QBItem, QBPDFable};
 use reqwest::Method;
+use tokio::io::AsyncWriteExt;
 
 use crate::client::Quickbooks;
 use crate::error::APIError;
 
-use super::{qb_request, QBResponse};
-
 #[async_trait]
 pub trait QBPDF: QBPDFable + QBItem {
-    async fn get_pdf(&self, qb: &Quickbooks<Authorized>) -> Result<Self, APIError> {
-        if !self.can_read() {
-            return Err(APIError::NoIdOnGetPDF);
+    async fn get_pdf_bytes(&self, qb: &Quickbooks<Authorized>) -> Result<Vec<u8>, APIError> {
+        let id = match self.id() {
+            Some(id) => id,
+            None => return Err(APIError::NoIdOnGetPDF),
+        };
+
+        let path = &format!("company/{}/{}/{}/pdf", qb.company_id, Self::qb_id(), id);
+        let url = qb.build_url(path, &None)?;
+        let headers = qb.build_headers("application/pdf")?;
+        let request = qb.build_request(&Method::GET, url, headers, &None::<Self>)?;
+
+        let resp = qb.http_client.execute(request).await?;
+
+        if !resp.status().is_success() {
+            return Err(APIError::BadRequest(resp.text().await?))
         }
 
-        let resp = qb_request!(
-            qb,
-            Method::POST,
-            &format!("company/{}/{}", qb.company_id, Self::qb_id()),
-            self,
-            None
-        );
-
-        let resp: QBResponse<Self> = resp.json().await?;
-
         log::info!(
-            "Successfully deleted {} with ID of {}",
+            "Successfully got PDF of {} with ID : {}",
             Self::name(),
-            resp.object.id().unwrap()
+            self.id().unwrap()
         );
 
-        Ok(resp.object)
+        Ok(resp.bytes().await?.into())
+    }
+
+    async fn save_pdf_to_file(
+        &self,
+        file_name: &str,
+        qb: &Quickbooks<Authorized>,
+    ) -> Result<(), APIError> {
+        let bytes = self.get_pdf_bytes(qb).await?;
+        let mut file = tokio::fs::OpenOptions::new()
+            .create(true)
+            .write(true)
+            .open(file_name)
+            .await?;
+        file.write(&bytes).await?;
+        log::info!(
+            "Successfully saved {} with ID : {}",
+            Self::name(),
+            self.id().unwrap()
+        );
+        Ok(())
     }
 }
 
