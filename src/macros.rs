cdd88229b71dfde50cf95e94aaa21b7ac79c3597
Exotik850 <kidkool850@gmail.com>
2023-08-22 17:04:43 -0500

    Almost done on restricting valid operators on macro

--- src/macros.rs
+++ src/macros.rs
@@ -2,6 +2,11 @@ pub use const_str::{concat, convert_ascii_case};
 
 #[macro_export]
 macro_rules! qb_where_clause {
+    (_OP =) => {" = '"};
+    (_OP like) => {" like '"};
+    (_OP in) => {" in '"};
+    (_OP $op:tt) => { compile_error!("Invalid Operator") };
+
     (_TYPECHECK $struct_name:ident, $($field:ident),+) => {
         {
             const _: () = {
@@ -14,13 +19,15 @@ macro_rules! qb_where_clause {
         }
     };
 
-    (_CLAUSE $($field:ident: $value:literal),+) => {
+    (_CLAUSE $($field:ident $op:tt $value:literal),+) => {
         {
             $crate::macros::concat!(
                 "where ",
                 $(
                     $crate::macros::convert_ascii_case!(upper_camel, stringify!($field)),
-                    " like '",
+                    ' ',
+                    stringify!($op),
+                    " '",
                     $value,
                     "' AND ",
                 )+
@@ -28,45 +35,48 @@ macro_rules! qb_where_clause {
         }
     };
 
-    (_CLAUSE $($field:ident: $value:expr),+) => {
+    (_CLAUSE $($field:ident $op:tt $value:expr),+) => {
         {
             let mut _values = String::new();
             $(
-                _values.push_str("WHERE ");
-                _values.push_str($crate::macros::convert_ascii_case!(upper_camel, stringify!($field)));
-                _values.push_str(" like '");
+                _values += "WHERE ";
+                _values += $crate::macros::convert_ascii_case!(upper_camel, stringify!($field));
+                _values += " ";
+                _values += stringify!($op);
+                _values += " '";
                 _values += &($value).to_string();
-                _values.push_str("' AND ".into());
+                _values += "' AND ";
             )+
-
             let _final_length = _values.len() - 5;
             _values.truncate(_final_length);
             _values
         }
     };
 
-    ($struct_name:ident | $($field:ident: $value:expr),+) => {
+    ($struct_name:ident | $($field:ident $op:tt $value:expr),+) => {
         {
             $crate::qb_where_clause!(_TYPECHECK $struct_name, $($field),+);
-            $crate::qb_where_clause!(_CLAUSE $($field : $value),+)
+            $crate::qb_where_clause!(_CLAUSE $($field $op $value),+)
         }
     };
 
-    ($struct_name:ident | $($field:ident : $value:expr),+ ; $($addon:literal),+) => {
+    ($struct_name:ident | $($field:ident $op:tt $value:expr),+ ; $($addon:literal),+) => {
         {
             $crate::qb_where_clause!(_TYPECHECK $struct_name, $($field),+);
-            let _CLAUSE = $crate::qb_where_clause!(_CLAUSE $($field : $value),+)
-            let _ADDON = $crate::macros::concat!($($addon),+);
-            format!("{_CLAUSE} {_ADDON}")
+            let _clause = $crate::qb_where_clause!(_CLAUSE $($field $op $value),+);
+            const _ADDON: &'static str = $crate::macros::concat!($($addon),+);
+            format!("{_clause} {_ADDON}")
         }
     }
 }
 
-fn _test() {
-    use quickbooks_types::Customer;
-    let _tes = format!("{}", 10);
-    let _query = {
-        crate::qb_where_clause!(_TYPECHECK Customer,id);
-        crate::qb_where_clause!(_CLAUSE id:20u8)
+#[macro_export]
+macro_rules! qb_query {
+    ($qb:expr, $struct_name:ident | $($field:ident $op:tt $value:expr),+) => {
+        $struct_name::query_single($qb, &$crate::qb_where_clause!($struct_name | $($field $op $value)+)).await
+    };
+
+    ($qb:expr, $struct_name:ident | $($field:ident $op:tt $value:expr),+ ; $($addon:literal),+) => {
+        $struct_name::query_single($qb, &$crate::qb_where_clause!($struct_name | $($field $op $value)+ ; $($addon)+)).await
     };
 }
