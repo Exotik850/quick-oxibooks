f1ce256d9b4f0efb29031ff09717ea896fd3697f
kidkool850@gmail.com <kidkool850@gmail.com>
2023-08-31 01:55:42 -0500

    Examples added

---
+++ examples/invoice_query.rs
@@ -0,0 +1,29 @@
+use quick_oxibooks::client::Quickbooks;
+use quick_oxibooks::error::APIError;
+use quick_oxibooks::qb_query;
+use quickbooks_types::Invoice;
+
+#[tokio::main]
+async fn main() -> Result<(), APIError> {
+    let mut args = std::env::args();
+
+    let token = args.next().expect("Missing Token! 1st Argument"); 
+    let company_id = args.next().expect("Missing Token! 2nd Argument");
+    let env = args.next().expect("Missing Environment! 3rd Argument");
+    let doc_number = args.next().expect("Missing DocNumber! 4th Argument");
+    let key = args.next().expect("Missing Cache Key! 5th Argument");
+
+    let env = match env.as_str() {
+        "production" => intuit_oxi_auth::Environment::PRODUCTION,
+        "sandbox" => intuit_oxi_auth::Environment::SANDBOX,
+        _ => panic!("Invalid environment"),
+    };
+    
+    let qb = Quickbooks::new_from_token(&token, &company_id, env, &key).await?;
+
+    let inv = qb_query!(&qb, Invoice | doc_number = &doc_number)?;
+
+    println!("{inv}");
+
+    Ok(())
+}
