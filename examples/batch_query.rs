b44d35d57230d2eacd2ca6175f37936461c8c901
Exotik850 <kidkool850@gmail.com>
2025-03-05 22:04:59 -0600

    Update example to better show batch capabilities

--- examples/batch_query.rs
+++ examples/batch_query.rs
@@ -2,28 +2,60 @@ use quick_oxibooks::{batch::BatchItemRequest, QBContext};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let transaction_nxrs: Vec<String> = std::env::args().skip(1).collect();
+    let mut transaction_nxrs: Vec<String> = vec![];
+    let mut access_token = None;
+    let mut flag = false;
+    for arg in std::env::args().skip(1) {
+        // transaction_nxrs.push(arg);
+        if flag {
+            access_token = Some(arg);
+            flag = false;
+            continue;
+        }
+        if arg.trim() == "--access_token" {
+            flag = true;
+            continue;
+        }
+        transaction_nxrs.push(arg);
+    }
+
     let client = reqwest::Client::new();
-    let qb = QBContext::new_from_env(quick_oxibooks::Environment::PRODUCTION, &client).await?;
+    let mut qb = QBContext::new_from_env(quick_oxibooks::Environment::PRODUCTION, &client).await?;
+
+    if let Some(token) = access_token {
+        qb = qb.with_access_token(token)
+    }
 
     let mut batch_items = Vec::new();
     for num in transaction_nxrs {
-        batch_items.push(BatchItemRequest::query(format!(
-            r#"select * from Invoice where DocNumber = '{num}'"#
-        )));
+        batch_items.push(BatchItemRequest::query(dbg!(format!(
+            r#"select * from SalesReceipt where DocNumber = '{num}'"#
+        ))));
     }
     let batch_resp = quick_oxibooks::batch::qb_batch(batch_items, &qb, &client).await?;
     for item in batch_resp {
         match item.item {
             quick_oxibooks::batch::BatchItem::QueryResponse(qr) => {
-                println!("{}: {:?}", item.b_id, qr.data);
+                let msg = qr
+                    .data
+                    .map(|d| format!("{d:?}"))
+                    .unwrap_or_else(|| "None".to_string());
+                println!("{}: {}", item.b_id, msg);
             }
             quick_oxibooks::batch::BatchItem::Fault(f) => {
-                println!("{}: {:?}", item.b_id, f.r#type);
+                println!("Error with {}: {:?}, ", item.b_id, f.r#type);
+                for fault in f.error {
+                    println!(
+                        "\t- {} : {}",
+                        fault.message,
+                        fault.detail.as_deref().unwrap_or("[[No Detail]]")
+                    );
+                }
             }
             _ => {}
         }
     }
 
+    // println!("{}", batch_resp);
     Ok(())
 }
